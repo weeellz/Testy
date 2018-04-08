b6caf0dcd785be29eab92052222c21557c89aad9
Alekseev Artem <a.artem060@gmail.com>
2018-04-08 21:17:32 +0300

    small refactoring (#1)

--- src/testrunner.c
+++ src/testrunner.c
@@ -1,33 +1,52 @@
+/*!
+ * @authors
+ * Ravil Bikbulatov
+ * fexolm
+ *
+ * @date 08.04.18.
+ *
+ * @todo reverse test order
+ */
+
 #include <stdlib.h>
-#include "testrunner.h"
+#include <testy.h>
 
-TestRunner *allocTestRunner(){
-    TestRunner *runner = malloc(sizeof(TestRunner));
-    runner->head = NULL;
-    return runner;
+typedef struct __TestNode {
+  struct __TestNode *next;
+  testy_function test;
+} TestNode;
+
+struct __TestRunner {
+  TestNode *head;
+};
+
+testy_Runner testy_allocRunner() {
+  testy_Runner runner = malloc(sizeof(struct __TestRunner));
+  runner->head = NULL;
+  return runner;
 }
 
-void destroyTestRunner(TestRunner *runner){
-    TestNode *node = NULL;
-    while(runner->head){
-        node = runner->head;
-        runner->head = node->next;
-        free(node);
-    }
-    free(runner);
+void testy_destoyRunner(testy_Runner runner) {
+  TestNode *node = NULL;
+  while (runner->head) {
+    node = runner->head;
+    runner->head = node->next;
+    free(node);
+  }
+  free(runner);
 }
 
-void addTestCase(TestRunner *runner, testCase test){
-    TestNode *node = malloc(sizeof(TestNode));
-    node->test = test;
-    node->next = runner->head;
-    runner->head = node;
+void testy_addCase(testy_Runner runner, testy_function test) {
+  TestNode *node = malloc(sizeof(TestNode));
+  node->test = test;
+  node->next = runner->head;
+  runner->head = node;
 }
 
-void run(TestRunner *runner){
-    TestNode *node = runner->head;
-    while (node){
-        node->test();
-        node = node->next;
-    }
+void testy_run(testy_Runner runner) {
+  TestNode *node = runner->head;
+  while (node) {
+    node->test(runner);
+    node = node->next;
+  }
 }
