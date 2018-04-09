07af1883abd1002ffc51b357aa8bb266ffb03097
Alekseev Artem <a.artem060@gmail.com>
2018-04-09 22:14:19 +0300

    double comparator (#5)

---
+++ include/comparators.h
@@ -0,0 +1,11 @@
+/*!
+ * @author fexolm 
+ * @date 09.04.18.
+ */
+#pragma once
+#include <math.h>
+
+const double testy_EPSILON = 1.0E-8;
+const double testy_SMALL_EPSILON = 1.0E-150;
+
+int testy_compareDouble(double lhs, double rhs);
