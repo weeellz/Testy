78f2d45e8eeb6feb62c08ad7fea136aa9857cc61
weeellz <weeellz12@gmail.com>
2018-04-09 22:35:30 +0300

    add double assert

--- include/assert.h
+++ include/assert.h
@@ -4,6 +4,8 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
+#include "comparators.h"
 
 enum {
   testy_SUCCESS = 0,
@@ -31,13 +33,19 @@ enum {
 #define testy_assert_string(x, op, y) do { \
   const char* _testy_x = (x); \
   const char* _testy_y = (y); \
-  testy_check(0 op strcmp(_testy_x, _testy_y), "Assertion '%s' failed: %s = %jd, %s = %jd", #x" "#op" "#y, #x, _testy_x, #y, _testy_y) \
+  testy_check(strcmp(_testy_x, _testy_y) op 0, "Assertion '%s' failed: %s = %s, %s = %s", #x" "#op" "#y, #x, _testy_x, #y, _testy_y) \
 } while(0)
 
 #define testy_assert_ptr(x, op, y) do { \
   const void* _testy_x = (x); \
   const void* _testy_y = (y); \
   testy_check(_testy_x op _testy_y, "Assertion '%s' failed: %s = %jd, %s = %jd", #x" "#op" "#y, #x, _testy_x, #y, _testy_y) \
+} while(0)
+
+#define testy_assert_double(x, op, y) do { \
+  double _testy_x = (x); \
+  double _testy_y = (y); \
+  testy_check(testy_compareDouble(_testy_x, _testy_y) op 0, "Assertion '%s' failed: %s = %lf, %s = %lf", #x" "#op" "#y, #x, _testy_x, #y, _testy_y) \
 } while(0)
 
 #define testy_assert_int_eq(x, y) testy_assert_int(x, ==, y)
@@ -53,6 +61,13 @@ enum {
 #define testy_assert_uint_le(x, y) testy_assert_uint(x, <=, y)
 #define testy_assert_uint_gt(x, y) testy_assert_uint(x, >, y)
 #define testy_assert_uint_ge(x, y) testy_assert_uint(x, >=, y)
+
+#define testy_assert_double_eq(x, y) testy_assert_double(x, ==, y)
+#define testy_assert_double_ne(x, y) testy_assert_double(x, !=, y)
+#define testy_assert_double_lt(x, y) testy_assert_double(x, <, y)
+#define testy_assert_double_le(x, y) testy_assert_double(x, <=, y)
+#define testy_assert_double_gt(x, y) testy_assert_double(x, >, y)
+#define testy_assert_double_ge(x, y) testy_assert_double(x, >=, y)
 
 #define testy_assert_string_eq(x, y) testy_assert_string(x, ==, y)
 #define testy_assert_string_ne(x, y) testy_assert_string(x, !=, y)
